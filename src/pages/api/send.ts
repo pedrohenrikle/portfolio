2f0736c9a098b9903aad9fc185ba6e28bcbdd774
pedrohenrikle <pedro.klein.sl@gmail.com>
2024-02-16 11:15:35 -0300

    fix: route of send.ts

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -8,8 +8,15 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://portfolio-lake-five-28.vercel.app',
+    )
+    res.setHeader('Access-Control-Allow-Methods', 'POST')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
     if (req.method !== 'POST') {
-      res.status(405).end() // Método não permitido
+      res.status(405).end()
       return
     }
 
