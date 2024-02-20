a57668207ab00f40f2dafc297f70eac9bec4fe6d
pedrohenrikle <pedro.klein.sl@gmail.com>
2024-02-20 20:48:02 -0300

    fix: some bugs

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  headers: {
-    Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
-  },
   baseURL: '/api',
 })
--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,43 +1,48 @@
-/* eslint-disable import/no-anonymous-default-export */
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { EmailTemplate } from '../../components/EmailTemplate'
 import { FormData } from '@/components/FormButton'
+
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(`${process.env.RESEND_API_KEY}`)
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
-    if (req.method !== 'POST') {
-      res.status(405).end()
-      return
-    }
+    const { email, name, message }: FormData = req.body
 
-    const { name, email, message }: FormData = req.body
-
-    const { data, error } = await resend.emails.send({
+    const response = await resend.emails.send({
       from: 'Portfolio <onboarding@resend.dev>',
       to: ['pedro.klein.sl@gmail.com'],
       subject: `Novo contato de: ${name}`,
-      react:
-        EmailTemplate({
-          name,
-          email,
-          message,
-        }) || '',
+      text: 'E-mail de contato',
+      react: EmailTemplate({ name, email, message }),
+      headers: {
+        'X-Entity-Ref-ID': '123456789',
+      },
+      tags: [
+        {
+          name: 'category',
+          value: 'confirm_email',
+        },
+      ],
     })
 
-    if (error) {
-      res.status(400).json(error)
-      return
+    if (response.data) {
+      // Se houver dados na resposta, encerramos a rota
+      return res.status(201).end()
+    } else if (response.error) {
+      // Se houver um erro na resposta, lançamos uma exceção
+      throw new Error(`Failed to send email: ${response.error.message}`)
+    } else {
+      // Se a resposta não tiver dados nem erro, consideramos como um erro desconhecido
+      throw new Error('Unknown error in email sending')
     }
-
-    res.status(200).json(data)
   } catch (error) {
-    console.error('Erro ao processar a requisição:', error)
-    res.status(500).json({ error: 'Erro interno do servidor' })
+    // Se ocorrer um erro durante o processamento, retornamos um erro interno do servidor
+    console.error(error)
+    return res.status(500).json({ error: 'Internal server error' })
   }
 }
